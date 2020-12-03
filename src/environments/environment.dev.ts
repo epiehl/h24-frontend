d36784e64e64833ba0c3eb24673c81849f9a96f6
piehle <1337EOS93#>
2020-12-03 14:15:08 +0100

    add buildspec.yaml

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://dev-api.h24-notifier.com',
+  resourceServerAllowedUrls: ['https://dev-api.h24-notifier.com']
+};
+
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.h24-notifier.com',
+  resourceServerAllowedUrls: ['https://api.h24-notifier.com']
 };
